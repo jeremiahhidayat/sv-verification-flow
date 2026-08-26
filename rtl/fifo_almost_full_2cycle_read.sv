ef935da166f846ffea5c0ac124f24679c627c796
jeremiahhidayat <Jeremiahhidayat@yahoo.com>
2026-08-26 10:49:25 -0400

    Add internal signals to fifo

--- rtl/fifo_almost_full_2cycle_read.sv
+++ rtl/fifo_almost_full_2cycle_read.sv
@@ -19,5 +19,14 @@ module fifo_almost_full_2cycle_read #(
     output logic [          WIDTH-1:0] rd_data
 );
 
+localparam int READ_LATENCY = 2;
+
+logic [WIDTH-1:0] ram[DEPTH];
+logic [WIDTH-1:0] rd_data_ram;
+logic [$clog2(DEPTH)-1:0] wr_addr_r, rd_addr_r;
+
+localparam int COUNT_WIDTH = $clog2(DEPTH) + 1;
+logic [COUNT_WIDTH-1:0] count_r, next_count, count_update;
+logic valid_wr, valid_rd;
 
 endmodule
